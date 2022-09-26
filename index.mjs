d1daf6392f6dc83b9ae4400e6be81562eaf0bdce
PonomareVlad <Git@PonomareVlad.ru>
2022-09-26 18:41:27 +0500

    Webhook setup

--- index.mjs
+++ index.mjs
@@ -1,4 +1,55 @@
-export const handler = async (bot = {}, {body} = {}, {json = _ => _} = {}) =>
+/**
+ * TeleBot instance
+ * @typedef {Object} TeleBot TeleBot instance
+ */
+
+/**
+ * Environment hosts
+ * @typedef {Object<string,string>} EnvHosts Environment hosts
+ */
+
+/**
+ * Getting the target host
+ * @param {EnvHosts} [hosts] Environment hosts
+ * @param {Object} [headers] Request HTTP headers
+ * @returns {String | undefined | null} Target host
+ */
+export const getHost = (hosts = {}, headers = {}) => hosts[process?.env?.VERCEL_ENV] || headers?.['x-forwarded-host']
+
+/**
+ * Webhook setup handler
+ * @param {TeleBot} bot
+ * @param {String} [path] Path to function
+ * @param {EnvHosts} [hosts] Environment hosts
+ * @param {Object} [headers] Request HTTP headers
+ * @param {Function} [json] Server JSON-response function
+ * @returns {Promise} Server response promise
+ */
+export const setWebhookHandler = async (bot = {}, path = '', hosts, {headers} = {}, {json = _ => _} = {}) =>
+    json(await bot?.setWebhook(`https://${getHost(hosts, headers)}/${path}`).catch(_ => _))
+
+/**
+ * Webhook setup handler factory
+ * @param {TeleBot} bot TeleBot instance
+ * @param {String} [path] Path to function
+ * @param {EnvHosts} [hosts] Environment hosts
+ * @return {Function: setWebhookHandler} Webhook setup handler
+ */
+export const setWebhook = (bot, path, hosts) => setWebhookHandler.bind(this, bot, path, hosts)
+
+/**
+ * Webhook handler
+ * @param {TeleBot} bot TeleBot instance
+ * @param {Object} [body] Request body
+ * @param {Function} [json] Server JSON-response function
+ * @returns {Promise} Server response promise
+ */
+export const startHandler = async (bot = {}, {body = {}} = {}, {json = _ => _} = {}) =>
     json(body?.update_id ? await bot?.receiveUpdates([body]) : {status: false})
 
-export default bot => handler.bind(this, bot)
+/**
+ * Webhook handler factory
+ * @param {TeleBot} bot TeleBot instance
+ * @return {Function: startHandler} Webhook handler
+ */
+export const start = bot => startHandler.bind(this, bot)
